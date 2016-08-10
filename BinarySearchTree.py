15d83bab7cbf4feba0407d364e32e534ff5f4a66
mhfarahani <mh.farahani@gmail.com>
2016-08-10 14:30:51 -0700

    insertNode corrected and traversal methods added

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -13,15 +13,17 @@ class Node(object):
             return False
         else:
             if self.item > new_item:
-                current_child = self.left_child
+                if self.left_child:
+                    return self.left_child.insertNode(new_item)
+                else:
+                    self.left_child = Node(new_item)
+                    return True
             else:
-                current_child = self.right_child
-            
-            if current_child:
-                return current_child.insertNode(new_item)
-            else:
-                current_child = Node(new_item)
-                return True
+                if self.right_child:
+                    return self.right_child.insertNode(new_item)
+                else:
+                    self.right_child = Node(new_item)
+                    return True
 
     def find(self,an_item):
         if self.item == an_item:
@@ -36,7 +38,19 @@ class Node(object):
                 return current_child.find(an_item)
             else:
                 return False
-            
+
+    def traversal(self,method):
+        if self:
+            if method == 'preorder':
+                print(self.item)
+            if self.left_child:
+                self.left_child.traversal(method)
+            if method == 'inorder':
+                print(self.item)
+            if self.right_child:
+                self.right_child.traversal(method)
+            if method == 'postorder':
+                print(self.item) 
 
 
 class BinaryTree(object):
@@ -56,8 +70,30 @@ class BinaryTree(object):
             return self.root.find(item)
         else:
             return False
-                
-        
+
+    def preorderTraversal(self):
+        print('-------------------')
+        print('Preorder Traversal:')
+        if self.root:
+            self.root.traversal('preorder')
+        print('-------------------')
+
+    def inorderTraversal(self):
+        print('-------------------')
+        print('Inorder Traversal:')
+        if self.root:
+            self.root.traversal('inorder')
+        print('-------------------')
+
+    def postorderTraversal(self):
+        print('-------------------')
+        print('Postorder Traversal:')
+        if self.root:
+            self.root.traversal('postorder')
+        print('-------------------')
+
+
+            
 
 
 
@@ -65,8 +101,12 @@ tree = BinaryTree()
 print(tree.insert(24))
 print(tree.insert(12))
 print(tree.insert(4))
-print(tree.insert(24))
+print(tree.insert(12))
 print(tree.insert(55))
+print(tree.insert(33))
 print('---')
 print(tree.isin(23))
-print(tree.isin(24))
+print(tree.isin(55))
+tree.preorderTraversal()
+tree.postorderTraversal()
+tree.inorderTraversal()
