8b5b67e3bc1c5c904d7062dd8b5d82a732d8c783
mhfarahani <mh.farahani@gmail.com>
2016-08-15 19:49:10 -0700

    Selection Sort was added

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -1,5 +1,5 @@
 ## Bubble Sort
-
+# O(n^2)
 def bubleSort(alist):
     length = len(alist)
     for i in range(length):
@@ -11,7 +11,20 @@ def bubleSort(alist):
         if sorted_flag:
             return alist
     return alist
-            
+
+## Selection Sort
+# O(n^2)
+def selectionSort(alist):
+    lenght = len(alist)
+    for i in range(lenght):
+        max_index = 0
+        for j in range(1,lenght-i):
+            if alist[max_index]<alist[j]:
+                max_index = j
+        if j != max_index:
+            alist[j],alist[max_index]=alist[max_index],alist[j]
+    return alist
+
 
 alist = [54,26,93,17,77,31,44,55,20]
-print(bubleSort(alist))
+print(selectionSort(alist))
